411dd2aaf69fcb4deb1f45ed96a144f5de8e3b1d
jfeito26 <jorgefeicar26@gmail.com>
2025-01-27 23:37:35 +0100

    Add files via upload

---
+++ Assets/SCRIPTS/MoveTowards.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowards : MonoBehaviour
+{
+    public float speed = 3f;
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = FindObjectOfType<PLAYERMOVEMENT>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newposition = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        transform.position = newposition;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(target);
+        }
+    }
+}
